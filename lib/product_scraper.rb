771102b67c8b2004eb9a3785251045d6706d9dc7
Azhar Shah <azharshahstl@yahoo.com>
2018-09-14 18:13:50 +0000

    developing scraper

--- lib/product_scraper.rb
+++ lib/product_scraper.rb
@@ -4,7 +4,13 @@ require "pry"
 class Scraper 
   
   def self.scrape_eggxtra_hot_deals 
-    doc = Nokogiri::HTML(open("https://www.newegg.com/"))
+    doc = Nokogiri::HTML(open("https://www.newegg.com/marketplace/deals?cm_sp=Homepage_MKPL-_--_-09142018"))
+    
+    doc.css(".promotion-box").each do |product|
+      product.css(".itemDescription").text
+      binding.pry
+    end
+  
   end
   
   
