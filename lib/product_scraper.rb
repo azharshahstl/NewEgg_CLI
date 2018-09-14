58f3e4a3762b0e45b245a820ffe937af7917443c
Azhar Shah <azharshahstl@yahoo.com>
2018-09-14 19:03:05 +0000

    added url, name and price keys to hash with css selectors

--- lib/product_scraper.rb
+++ lib/product_scraper.rb
@@ -6,8 +6,11 @@ class Scraper
   def self.scrape_eggxtra_hot_deals 
     doc = Nokogiri::HTML(open("https://www.newegg.com/marketplace/deals?cm_sp=Homepage_MKPL-_--_-09142018"))
     
-    doc.css(".promotion-box").each do |product|
-      product.css(".itemDescription").text
+    doc.css(".promotion-details").each do |product|
+      product_hash = {}
+      product_hash[:url] = product.css("a").attribute("href").value
+      product_hash[:name] = product.css(".itemDescription").text
+      product_hash[:price] = product.css(".price-current").text
       binding.pry
     end
   
