a1a96d324f9440e57390dfcb31f3337dbaf1b936
Azhar Shah <azharshahstl@yahoo.com>
2018-09-18 18:25:53 +0000

    added to Product class

--- lib/products.rb
+++ lib/products.rb
@@ -2,11 +2,15 @@ class Product
   
   attr_accessor :url, :name, :price, :item_action
   
+  @all = []
+  
   def initialize(product_hash)
     @url = product_hash[:url]
     @name = product_hash[:name]
     @price = product_hash[:price] 
     @item_action = product_hash[:item_action]
+    
+    @all << self
     
   
 end
