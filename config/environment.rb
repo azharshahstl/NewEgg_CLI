522e88caf73cad8e8e8fcf9e82ec5230b719243e
Azhar Shah <azharshahstl@yahoo.com>
2018-10-16 22:33:08 +0000

    creating confi dir and envirnoment file

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require "nokogiri"
+require "colorize" 
+require "open-uri" 
+require "pry" 
+
+require_relative "../lib/product_scraper.rb" 
+require_relative "../lib/products.rb" 
+require_relative "../NewEgg_CLI.rb" 
