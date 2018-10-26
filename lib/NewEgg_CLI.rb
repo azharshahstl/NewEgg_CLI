2ceb8facf184b98fd55ceca740faee48293ca86a
Azhar Shah <azharshahstl@yahoo.com>
2018-10-26 16:52:15 +0000

    CLI scraping of third page

--- lib/NewEgg_CLI.rb
+++ lib/NewEgg_CLI.rb
@@ -29,34 +29,34 @@ class NewEggCLI
         counter += 1
       end
     end
-    puts "Enter the number of the product you would like more info on: "
+    #puts "Enter the number of the product you would like more info on: "
   end
   
   def displayCLI_options 
     puts "Would you like to see the Products available on NewEggs Eggxtra hot deals? 1 if yes, 2 if no. "
     input = gets.strip.to_i
     if input == 2 
-      puts "Sorry you are not interested now, come back again when you are :)."
+      puts "Sorry you are not interested now, come back again when you are. :)"
     elsif input == 1
       puts "There are currently #{Product.all.size} products to view."
       puts "How many products would you like to view?"
       input = gets.strip.to_i 
         if input >= 1 && input <= Product.all.size
         display_products(input)
+        puts "Enter the number of the product you would like more info on: "
+        input = gets.strip.to_i
+        product = Product.all[input - 1]
+      Scraper.scrape_more_product_info(product) if product.info = nil
+      display_product(product)
         else 
           puts "I am sorry, your input was ouside the range of products found."
+          displayCLI_options
         end
     else 
       puts "I am sorry, I did not understand what you entered."
       displayCLI_options
-      #get input(products_viewed) and validate it completely.
-      #display_products(products_viewed)
-      
-      input = gets.strip.to_i 
-      #validate line 46 input.
-      product = Product.all[input - 1]
-      Scraper.scrape_more_product_info(product) if product.info = nil
-      display_product(product)
+   
+     
       #prompt user and do what they want to do. 
     end
   end 
